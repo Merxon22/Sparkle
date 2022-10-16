09809a1f61615265daaf8ee8dd10020c98c1203a
Hao Yuan <merxon22@gmail.com>
2022-10-16 00:54:21 -0700

    bug fix

--- Temp.py
+++ Temp.py
@@ -8,18 +8,25 @@ pygame.display.set_caption("SPARKLEs")
 running=True
 earth=pygame.image.load("Earth.jpg")
 earth_rect=earth.get_rect()
+earth_pos = (150, 250)
+earth_check_distance = 65
 
 score=0
+
+def check_cursor_distance_from_star(cursor_pos, earth_pos):
+    earth_center = (earth_pos[0] + earth_rect.height/2, earth_pos[1] + earth_rect.width/2)
+    return ((cursor_pos[0]-earth_center[0])**2 + (cursor_pos[1]-earth_center[1])**2)**0.5
+
 while running:
     screen.fill((250, 250, 000))
-    screen.blit(earth, (150, 250))
+    screen.blit(earth, earth_pos)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
         if event.type==pygame.MOUSEBUTTONDOWN:
             pos=pygame.mouse.get_pos()
             #print(pos)
-            if earth.get_rect().collidepoint(pos):
+            if check_cursor_distance_from_star(pos, earth_pos) <= earth_check_distance:
                 score+=1
     #earth_rect2=earth_rect.move(speed)
     #if earth_rect2.left < 0 or earth_rect2.right > 400:
@@ -33,3 +40,4 @@ while running:
     surface=font.render(str(score), True, (255, 0, 0))
     screen.blit(surface, (200, 100))
     pygame.display.flip()
+
